e33270e7892e6e1b245b1cd0db495d2fa3498508
JShaikh <jelonmusk@gmail.com>
2020-08-10 23:39:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey Jude, Nice to Meet you !
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Take a  React App and Make it Better
+        </a>
+      </header>
+    </div>
+  );
+
+  
+}
+
+export default App;
